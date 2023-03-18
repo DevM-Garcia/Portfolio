ff430cc819b4a5c623ce88d7367fb0b5aaccbac8
Matheus Garcia <115594762+DevM-Garcia@users.noreply.github.com>
2023-03-18 16:44:03 -0300

    atualização do js

--- main.js
+++ main.js
@@ -5,5 +5,17 @@ $(document).ready(function(){
         }else{
             $('.nav').removeClass("sticky");
         }
+        if(this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show")
+        }
+    });
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop:0});
+    })
+    $('.icon-menu').click(function(){
+        $('.nav .menu').toggleClass("active");
+        $('icon-menu i').toggleClass("active");
     });
 });
