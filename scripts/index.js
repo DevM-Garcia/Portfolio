aba685e338fdc7b763ae94c3db59edaba20882ad
Matheus Garcia <115594762+DevM-Garcia@users.noreply.github.com>
2023-08-18 13:45:54 -0300

    finalizando portfólio

--- scripts/index.js
+++ scripts/index.js
@@ -46,4 +46,21 @@ themeButton.addEventListener('click', () => {
     // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
+});
+
+/*=============== Scroll reveal animation ===============*/
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
 })
+sr.reveal(`.profile__border`)
+sr.reveal(`.profile__name`, { delay: 500 })
+sr.reveal(`.profile__profession`, { delay: 600 })
+sr.reveal(`.profile__social`, { delay: 700 })
+sr.reveal(`.profile__info-group`, { interval: 100, delay: 700 })
+sr.reveal(`.profile__buttons`, { delay: 800 })
+sr.reveal(`.filters__content`, { delay: 900 })
+sr.reveal(`.filters`, { delay: 600 })
